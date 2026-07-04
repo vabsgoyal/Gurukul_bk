318801d51ab1dbb5e6f58fe2dc4f813fb17cebd8
Vaibhav Goyal <vaibhavgoyal@Vaibhavs-MacBook-Pro.local>
2026-07-04 16:14:18 +0900

    Add Spring Boot backend

---
+++ src/main/java/com/digitalschool/digital_school_backend/DigitalSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digitalschool.digital_school_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSchoolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalschool/digital_school_backend/DigitalSchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalschool.digital_school_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
